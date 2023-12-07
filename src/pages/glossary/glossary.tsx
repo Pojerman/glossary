4f39e4559f98f5cc6596785c37b90a716c6bf986
Danil <45513393+Pojerman@users.noreply.github.com>
2023-12-07 18:43:18 +0300

    init

---
+++ src/pages/glossary/glossary.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Glossary(): React.JSX.Element {
+  return(
+    <>
+      <div>Hello</div>
+    </>
+  )
+}
---
+++ src/pages/semantic-graph/semantic-graph.tsx
@@ -0,0 +1,5 @@
+export default function SemanticGraph() {
+  return(
+    <div></div>
+  )
+}
